e15bdfe6eb42645778e0fc175e2752b280523e52
insu12021202 <insu12021202@gmail.com>
2023-10-21 13:01:30 +0900

    2023-10-20 DFS/BFS 문제풀이

---
+++ Problems/BFS/1697.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const file = process.platform === "linux" ? "dev/stdin" : "./1697.txt";
+let input = fs.readFileSync(file).toString().trim().split("\n");
+input = [...input].map((x) => x.replace("\r", ""));
+
+// 문제명: 숨바꼭질 (실버 1)
+
+let [n, k] = input[0].split(" ").map(Number);
+
+let visited = Array.from({ length: 100000 }, () => 0);
+
+const bfs = (start) => {
+    const q = [];
+    q.push(start);
+
+    while (q.length !== 0) {
+        let v = q.shift();
+        if (v == k) {
+            return visited[v];
+        }
+
+        for (let next of [v + 1, v - 1, v * 2]) {
+            if (next < 0 || next > 100000) continue;
+            if (visited[next] == 0) {
+                q.push(next);
+                visited[next] = visited[v] + 1;
+            }
+        }
+    }
+};
+
+console.log(bfs(n));
