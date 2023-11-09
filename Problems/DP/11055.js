02b6f0dc1f35a946ba18eb96b16abe1eb3dd53da
insu12021202 <insu12021202@gmail.com>
2023-11-09 16:15:55 +0900

    2023-11-09 DP 문제 풀이

---
+++ Problems/DP/11055.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const file = process.platform === "linux" ? "dev/stdin" : "./11055.txt";
+let input = fs.readFileSync(file).toString().trim().split("\n");
+input = [...input].map((x) => x.replace("\r", ""));
+
+// 문제명: 가장 큰 증가하는 부분 수열 (실버2)
+
+let n = Number(input[0]);
+let arr = input[1].split(" ").map(Number);
+
+let dp = Array.from({ length: n }, () => 1);
+dp[0] = arr[0];
+
+for (let i = 1; i < n; i++) {
+    dp[i] = arr[i];
+    for (let j = 0; j < i; j++) {
+        if (arr[i] > arr[j]) {
+            dp[i] = Math.max(dp[i], dp[j] + arr[i]);
+        }
+    }
+}
+// console.log(dp);
+console.log(Math.max(...dp));
